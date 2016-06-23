65e7455547b6097218f3146f36ff41d85a34f54b
Jaseem Abid <jaseemabid@gmail.com>
2016-06-24 02:08:27 +0530

    Local files

--- src/hello.cc
+++ src/hello.cc
@@ -10,7 +10,7 @@ int main() {
     leveldb::DB* db;
     leveldb::Options options;
     options.create_if_missing = true;
-    leveldb::Status status = leveldb::DB::Open(options, "/tmp/testdb", &db);
+    leveldb::Status status = leveldb::DB::Open(options, "tmp/testdb", &db);
     assert(status.ok());
     return 0;
 }
